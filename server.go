a0b7e9d50193dee9ebf8288a3f645b330f3cc777
Ariel Pierot <ariel.pierot@dcomp.ufs.br>
2019-03-22 02:17:09 +0000

    mudanças no código

--- server.go
+++ server.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
+	"github.com/golang/protobuf/proto"
 	"net"
 	"os"
 	"strings"
 )
 
-var Version = "TMQP 0.1\n"
-
 func main() {
 
-	ln, err := net.Listen("tcp", ":4000")
+	ln, err := net.Listen("tcp", ":7788")
 	checkError(err)
 	fmt.Println("ONLINE! WAITING NEW CONNECTIONS...")
 	for {
@@ -24,19 +24,26 @@ func main() {
 	os.Exit(0)
 }
 
+const (
+	DELIMITER byte = '\n'
+	TYPE      uint = 1
+	SIZE      uint = 4
+	VERSION        = "TMQP 0.1\n"
+)
+
 func handleConn(conn net.Conn) {
 
-	fmt.Println("NEW CONNECTION!")
+	fmt.Println("NEW CONNECTION RECEIVED...")
 
 	msg, err := bufio.NewReader(conn).ReadString('\n')
 
-	if strings.Compare(msg, Version) == 0 {
+	if strings.Compare(msg, VERSION) == 0 {
 
 		_, err = conn.Write([]byte("START\n"))
 		msg, err = bufio.NewReader(conn).ReadString('\n')
 
 		if strings.Compare(msg, "START OK\n") == 0 {
-			fmt.Println("CONNECTION ESTABLISHED")
+			fmt.Println("CONNECTION ESTABLISHED...")
 			go messageParser(conn)
 		}
 
@@ -48,13 +55,67 @@ func handleConn(conn net.Conn) {
 
 func messageParser(conn net.Conn) {
 
-	fmt.Println("NEW THREAD CREATED!")
+	fmt.Println("NEW THREAD CREATED TO LISTEN AND SERVE...")
+
+	// status := make(chan bool)
+
+	for {
+		readPackage(conn)
+	}
+
+}
+
+func readPackage(conn net.Conn) {
+
+	type_byte := make([]byte, TYPE)
+	_, err := conn.Read(type_byte[0:TYPE])
+
+	if err == nil {
+		type_package := int(type_byte[0])
+
+		fmt.Println("RECEBENDO PACOTE...")
+		fmt.Println("TYPE: ", type_package)
+
+		size_bytes := make([]byte, SIZE)
+		_, err := conn.Read(size_bytes[0:SIZE])
+
+		if err != nil {
+			fmt.Println("ERROR - SIZE: ", err)
+		} else {
+
+			size_package := binary.LittleEndian.Uint32(size_bytes[0:SIZE])
+
+			content_bytes := make([]byte, size_package)
+
+			fmt.Println("SIZE: ", size_package)
+
+			_, err := conn.Read(content_bytes[0:size_package])
+
+			if err != nil {
+				fmt.Println("ERROR - CONTEUDO: ", err)
+			} else {
+				if type_package == 1 {
+					newQueue := &QueueDeclare{}
+					err := proto.Unmarshal(content_bytes, newQueue)
+					if err != nil {
+						fmt.Println("ERROR - UNMARSHAL: ", err)
+					} else {
+						fmt.Println("NEW QUEUE: ", newQueue.GetName())
+						// status <- true
+					}
+				}
+			}
+		}
+
+	} else {
+		fmt.Println("ERROR - TYPE: ", err)
+	}
 
 }
 
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "FATAL ERROR: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "FATAL ERROR: %s \n", err.Error())
 		os.Exit(1)
 	}
 }
